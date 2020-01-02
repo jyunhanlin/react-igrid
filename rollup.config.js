fefd33c5f31bf69d9d1fff4835f17f9880e97374
Jyun-Han Lin <jyunhanlin@gmail.com>
2020-01-02 15:28:29 +0800

    remove UMD config in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
-import external from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
-// import { uglify } from "rollup-plugin-uglify";
-import packageJSON from "./package.json";
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+import packageJSON from './package.json';
 
-const input = "./src/index.js";
-const minifyExtension = pathToFile => pathToFile.replace(/\.js$/, ".min.js");
+const input = './src/index.js';
+const minifyExtension = pathToFile => pathToFile.replace(/\.js$/, '.min.js');
 
 export default [
   // CommonJS
@@ -15,91 +14,47 @@ export default [
     input,
     output: {
       file: packageJSON.main,
-      format: "cjs"
+      format: 'cjs',
     },
     plugins: [
       babel({
-        exclude: "node_modules/**"
-      }),
-      external(),
-      resolve(),
-      commonjs()
-    ]
-  },
-  // UMD
-  {
-    input,
-    output: {
-      file: packageJSON.browser,
-      format: "umd",
-      name: "reactIGrid",
-      globals: {
-        react: "React",
-        "@emotion/styled": "styled",
-        "@emotion/core": "core"
-      }
-    },
-    plugins: [
-      babel({
-        exclude: "node_modules/**"
-      }),
-      external(),
-      resolve(),
-      commonjs()
-    ]
-  },
-  {
-    input,
-    output: {
-      file: minifyExtension(packageJSON.browser),
-      format: "umd",
-      name: "reactIGrid",
-      globals: {
-        react: "React",
-        "@emotion/styled": "styled",
-        "@emotion/core": "core"
-      }
-    },
-    plugins: [
-      babel({
-        exclude: "node_modules/**"
+        exclude: 'node_modules/**',
       }),
       external(),
       resolve(),
       commonjs(),
-      terser()
-    ]
+    ],
   },
   // ES
   {
     input,
     output: {
       file: packageJSON.module,
-      format: "es"
+      format: 'es',
     },
     plugins: [
       babel({
-        exclude: "node_modules/**"
+        exclude: 'node_modules/**',
       }),
       external(),
       resolve(),
-      commonjs()
-    ]
+      commonjs(),
+    ],
   },
   {
     input,
     output: {
       file: minifyExtension(packageJSON.module),
-      format: "es"
+      format: 'es',
     },
     plugins: [
       babel({
-        exclude: "node_modules/**"
+        exclude: 'node_modules/**',
       }),
       external(),
       resolve(),
       commonjs(),
-      terser()
-    ]
-  }
+      terser(),
+    ],
+  },
 ];
